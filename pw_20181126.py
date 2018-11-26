c5a03677ec6fb9721ec330ee470e99283b71c522
eda3 <gerorin1010@gmail.com>
2018-11-27 03:20:42 +0900

    pyperclipを追加し、パスワードをクリップボードにコピーする機能追加

--- pw_20181126.py
+++ pw_20181126.py
@@ -1,4 +1,4 @@
-#! python3
+####! python3
 # pw.py - パスワード管理プログラム(脆弱性あり)
 
 PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
@@ -6,10 +6,18 @@ PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'luggage':' VmALvQyKAxiVH5G8v01if1MLZF3sdt'}
 
 import sys
+import pyperclip
+
 if len(sys.argv) <2:
-  print('使い方: python pw.py [アカウント名')
+  print('使い方: python pw.py [アカウント名]')
   print('パスワードをクリップボードにコピーします')
   sys.exit()
 
 account = sys.argv[1] # 最初のコマンドライン引数がアカウント名
 print(account)
+
+if account in PASSWORDS:
+  pyperclip.copy(PASSWORDS[account])
+  print(account + 'のパスワードをクリップにコピーしました')
+else:
+  print(account + 'というアカウント名はありません')
