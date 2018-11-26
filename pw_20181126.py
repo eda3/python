dd6e18fb4f629ccd41718460a2d2ed261bbd501a
eda3 <gerorin1010@gmail.com>
2018-11-26 15:07:37 +0900

    パスワード管理プログラム 引数チェックまで作成

---
+++ pw_20181126.py
@@ -0,0 +1,15 @@
+#! python3
+# pw.py - パスワード管理プログラム(脆弱性あり)
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage':' VmALvQyKAxiVH5G8v01if1MLZF3sdt'}
+
+import sys
+if len(sys.argv) <2:
+  print('使い方: python pw.py [アカウント名')
+  print('パスワードをクリップボードにコピーします')
+  sys.exit()
+
+account = sys.argv[1] # 最初のコマンドライン引数がアカウント名
+print(account)
