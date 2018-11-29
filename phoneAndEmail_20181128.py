96ca825d24abaafae2d32158d6885aa3e74980f3
eda3 <gerorin1010@gmail.com>
2018-11-29 14:38:33 +0900

    E-mailの正規表現マッチ追加

--- phoneAndEmail_20181128.py
+++ phoneAndEmail_20181128.py
@@ -12,6 +12,14 @@ phone_regrex = re.compile(r'''(
   (\s*(ext|x|ext.)\s*(\d{2,5}))?
 )''', re.VERBOSE)
 
+# 電子メールの正規表現
+email_regex = re.compile(r'''(
+  [a-zA-Z0-9._%+-]+ # ユーザ名
+  @                 # @ 記号
+  [a-zA-Z0-9.-]+    # ドメイン名
+  (\.[a-zA-Z]{2,4}) # ドットなんとか
+)''', re.VERBOSE)
+
 text = str(pyperclip.paste())
 matches = []
 for groups in phone_regrex.findall(text):
@@ -19,3 +27,6 @@ for groups in phone_regrex.findall(text):
   if groups[8] != '':
     phone_num += ' x' + groups[8]
   matches.append(phone_num)
+
+for groups in email_regex.findall(text):
+  matches.append(groups[0])
