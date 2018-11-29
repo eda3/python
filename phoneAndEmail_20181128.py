61c3698ce685ea45a10569671311f17d94472f9f
eda3 <gerorin1010@gmail.com>
2018-11-29 13:40:11 +0900

    クリップボードから電話番号を検索する処理追加

--- phoneAndEmail_20181128.py
+++ phoneAndEmail_20181128.py
@@ -11,3 +11,9 @@ phone_regrex = re.compile(r'''(
   (\d{4}) #加入者番号
   (\s*(ext|x|ext.)\s*(\d{2,5}))?
 )''', re.VERBOSE)
+
+text = str(pyperclip.paste())
+matches = []
+for groups in phone_regrex.findall(text):
+  phone_num = '-'.join([groups[1], groups[3], groups[5]])
+  print(phone_num)
