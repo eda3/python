2153e3e13d7a2cb24f70688c20ad2913e25ee589
eda3 <gerorin1010@gmail.com>
2018-12-03 20:30:09 +0900

    logger追加

--- mapIT_20181203.py
+++ mapIT_20181203.py
@@ -2,6 +2,16 @@
 # mapIT.y - コマンドラインやクリップボードに指定した住所の地図を開く
 
 import webbrowser, sys
+
+from logging import getLogger, StreamHandler, DEBUG
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
 if len(sys.argv) > 1:
   # コマンドラインから住所を取得
   address = ' '.join(sys.argv[1:])
+  logger.debug('address:' + address)
