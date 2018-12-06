ca18a1afe8d0a659aad0472be5e88f55f905da19
eda3 <gerorin1010@gmail.com>
2018-12-06 19:02:15 +0900

    結果をもとにブラウザからタブを開く処理を追加

--- lucky_20181203.py
+++ lucky_20181203.py
@@ -18,3 +18,8 @@ res.raise_for_status()
 # 上位の検索結果のリンクを取得する
 soup = bs4.BeautifulSoup(res.text)
 link_elems = soup.select('.r a')
+
+# 各結果をブラウザのタブで開く
+num_open = min(5, len(link_elems))
+for i in range(num_open):
+  webbrowser.open('http://google.com' + link_elems[i].get('href'))
