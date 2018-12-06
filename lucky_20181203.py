a7f1dcb914179ac4a1287617b83f1dda1bb4709d
eda3 <gerorin1010@gmail.com>
2018-12-06 18:52:34 +0900

    検索結果のリンクを取得する処理追加

--- lucky_20181203.py
+++ lucky_20181203.py
@@ -14,3 +14,7 @@ import requests, sys, webbrowser, bs4
 print('Googling...')
 res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
+
+# 上位の検索結果のリンクを取得する
+soup = bs4.BeautifulSoup(res.text)
+link_elems = soup.select('.r a')
