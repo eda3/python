201cf7d168af9be4495c3d54d2f4990f29d5177f
eda3 <gerorin1010@gmail.com>
2018-12-06 19:16:27 +0900

    xkcdを走査する処理追加

--- downloadXkcd_20181206.py
+++ downloadXkcd_20181206.py
@@ -1,2 +1,17 @@
 #! python3
 # downloadXkcd.py - XKCDコミックを一つずつダウンロードする
+
+import requests, os, bs4
+from logging import getLogger, StreamHandler, DEBUG
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+logger.propagate = False
+
+url = 'http://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+while not url.endswith('#'):
+  break
