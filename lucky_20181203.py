5d9a00796974c7ecfbe41dafc6dc4863a2016ec4
eda3 <gerorin1010@gmail.com>
2018-12-06 15:29:06 +0900

    Google検索用文字列取得

--- lucky_20181203.py
+++ lucky_20181203.py
@@ -10,3 +10,7 @@ logger.addHandler(handler)
 logger.propagate = False
 
 import requests, sys, webbrowser, bs4
+
+print('Googling...')
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
