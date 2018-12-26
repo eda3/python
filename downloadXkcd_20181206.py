710031dd6c02656e5a53d92ef435c381ce0e1da9
eda3 <gerorin1010@gmail.com>
2018-12-26 09:50:22 +0900

    ページDL処理追加

--- downloadXkcd_20181206.py
+++ downloadXkcd_20181206.py
@@ -14,4 +14,9 @@ url = 'http://xkcd.com'
 os.makedirs('xkcd', exist_ok=True)
 
 while not url.endswith('#'):
-  break
+  # ページをダウンロードする
+  print('ページをダウンロード中 {}...'.format(url))
+  res = requests.get(url)
+  res.raise_for_status()
+
+  soup = bs4.BeautifulSoup(res.text)
