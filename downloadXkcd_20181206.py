d75a7c4f1af12c2f78c2ecb23ef89ad6aac6ff37
eda3 <gerorin1010@gmail.com>
2018-12-27 20:01:09 +0900

    タブ幅を4に変更

--- downloadXkcd_20181206.py
+++ downloadXkcd_20181206.py
@@ -1,22 +1,17 @@
 #! python3
 # downloadXkcd.py - XKCDコミックを一つずつダウンロードする
 
-import requests, os, bs4
-from logging import getLogger, StreamHandler, DEBUG
-logger = getLogger(__name__)
-handler = StreamHandler()
-handler.setLevel(DEBUG)
-logger.setLevel(DEBUG)
-logger.addHandler(handler)
-logger.propagate = False
+import requests
+import os
+import bs4
 
 url = 'http://xkcd.com'
 os.makedirs('xkcd', exist_ok=True)
 
 while not url.endswith('#'):
-  # ページをダウンロードする
-  print('ページをダウンロード中 {}...'.format(url))
-  res = requests.get(url)
-  res.raise_for_status()
+    # ページをダウンロードする
+    print('ページをダウンロード中 {}...'.format(url))
+    res = requests.get(url)
+    res.raise_for_status()
 
-  soup = bs4.BeautifulSoup(res.text)
+    soup = bs4.BeautifulSoup(res.text)
