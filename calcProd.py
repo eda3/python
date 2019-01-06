6448bea541e6d7903354f476e5fba293a426b572
eda3 <gerorin1010@gmail.com>
2019-01-06 12:36:23 +0900

    A script to find the production of 1 to 99,999

---
+++ calcProd.py
@@ -0,0 +1,17 @@
+#! python3
+import time
+
+
+def calc_prod():
+    # Obtain the product of 1 to 99,999
+    product = 1
+    for i in range(1, 100000):
+        product = product * i
+    return product
+
+
+start_time = time.time()
+prod = calc_prod()
+end_time = time.time()
+print('計算結果は{}桁です。'.format(len(str(prod))))
+print('計算結果は{}秒でした。'.format(end_time - start_time))
