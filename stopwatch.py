f126cffceca789a644651ba2d8254786c2a214dd
eda3 <gerorin1010@gmail.com>
2019-01-06 15:29:42 +0900

    add simple stopwatch script

--- stopwatch.py
+++ stopwatch.py
@@ -12,3 +12,17 @@ print('Start')
 start_time = time.time()  # Program and start timeof first lap
 last_time = start_time
 lap_num = 1
+
+# Measure the lap time
+try:
+    while True:
+        input()
+        now = time.time()
+        lap_time = round(now - last_time, 2)
+        total_time = round(now - start_time, 2)
+        print('Lap #{}: {} ({})'.format(lap_num, total_time, lap_time), end='')
+        lap_num += 1
+        last_time = now  # Reset lap time
+except KeyboardInterrupt:
+    # Ctrl-C exceptions and suppress error messages
+    print('\nEnd.')
